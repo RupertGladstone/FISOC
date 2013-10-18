7fb3b37deb402932e683c55eb3e6c9810c6f8ce6
RupertGladstone <RupertGladstone1972@gmail.com>
2013-10-18 17:35:52 +1100

    Tidying up python script for simple Elmer/Ice mesh generation

--- examples/FISOC_mesh1a_prep.py
+++ examples/FISOC_mesh1a_prep.py
@@ -4,15 +4,29 @@ import os, sys, numpy
 # create DEM
 print "\ncreating DEM..."
 # first some parameters used in the DEM and the grid creation
-nx   = 20
 ny   = 4
-xlim = 500000. # in metres
+nz   = 6
 ylim =  20000. # in metres
+
+nx   = 20
+xlim = 500000. # in metres
+bedShape = "linear" 
+
+#nx   = 40
+#xlim = 1700000. # in metres
+#bedShape = "MM_overdeepened" 
+
 DEM_dir   = "FISOC_mesh1a_DEM"
 surfFile  = DEM_dir+"/surf.xyz"
 bedFile   = DEM_dir+"/bed.xyz"
 gridName2D = "FISOC_mesh1a_2D"
 gridName3D = "FISOC_mesh1a_3D"
+
+# valid bedshapes:
+# linear - downsloping towards ocean
+# MM_overdeepened - MISMIP polynomial overdeepened bed (designed for 1800km domain but seems fine with 1700km)
+# b(x) = - [ 729 - 2184.8(x/750km)^2 + 1031.72(x/750km)^4 - 151.72(x/750km)^6 ]m
+
 
 if DEM_dir not in os.listdir('.'):
     ret = os.mkdir(DEM_dir)
@@ -22,7 +36,14 @@ xAxis = numpy.linspace(0,xlim,nx+1)
 yAxis = numpy.linspace(0,ylim,ny+1)
 bedrock  = numpy.zeros([nx+1, ny+1])
 surface  = numpy.zeros([nx+1, ny+1])
-surface[:,:] = 20000.
+if bedShape == "linear":
+    bedrock[:,0] = numpy.linspace(500.,-500*1000.,nx+1)
+elif bedShape == "MM_overdeepened":
+    bedrock[:,0] =  -  729. - 2184.8*(xAxis[:]/750000.)**2 + 1031.72*(xAxis[:]/750000.)**4 - 151.72*(xAxis[:]/750000.)**6 
+else:
+    sys.exit("ERROR: bedShape not recognised")
+bedrock[:,:] = numpy.transpose(numpy.tile(bedrock[:,0],[ny+1,1]))
+surface[:,:] = bedrock[:,:]+1000.
 surf_out = open(surfFile,'w')
 bed_out  = open(bedFile,'w')
 for x in range(nx+1):
@@ -57,7 +78,7 @@ else:
 
 # create 3D mesh (extrude 2D mesh using DEM)
 print "\ncreating 3D mesh..."
-cmd = "ExtrudeMesh "+gridName2D+" "+gridName3D+" 6 10. 1 0 0 0 1 "+DEM_dir+" 1000. 1.5 -999999. > ExtrudeMesh.log"
+cmd = "ExtrudeMesh "+gridName2D+" "+gridName3D+" "+str(nz)+" 10. 1 0 0 0 1 "+DEM_dir+" 1000. 1.5 -999999. > ExtrudeMesh.log"
 print "\nstarting ExtrudeMesh with following command :\n",cmd
 ret = os.system(cmd)
 if ret == 0.:
