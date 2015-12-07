ef57ae5b474797f487ec28e3a064c88046bbfe93
RupertGladstone <rupertgladstone1972@gmail.com>
2015-12-07 17:27:44 +0200

    pp minor update for viewing Tb

--- FISOC_pp/FISOC_ROMS_check.py
+++ FISOC_pp/FISOC_ROMS_check.py
@@ -8,11 +8,11 @@ import matplotlib as mpl
 # (there should be no difference if the FISOC reformatting of vars to be ESMF 
 # friendly is working)
 
-timestep = 630
+timestep = 1000
 
-ROMS_grp  = Dataset("ocean_his.nc", "r")
+ROMS_grp  = Dataset("../ocean_his.nc", "r")
 
-FISOC_ncdfName = "FISOC_OM_exp_t"+str(timestep/10)+".nc"
+FISOC_ncdfName = "../FISOC_OM_exp_t"+str(timestep/10)+".nc"
 FISOC_grp = Dataset(FISOC_ncdfName, "r")
 
 #rootgrp = Dataset("FISOC_OM_exp_t138.nc", "r", format="NETCDF4")
@@ -37,38 +37,46 @@ ROMS_dBdt_l0 = ROMS_dBdt_l0[timestep,:,:]
 ROMS_z_l0 = ROMS_grp.variables["draft"]
 ROMS_z_l0 = ROMS_z_l0[timestep,:,:]
 
-ROMS_temperature_l0 = ROMS_grp.variables["temp"]
-#print ROMS_temperature_l0.shape
-ROMS_temperature_lx = ROMS_temperature_l0[timestep,10,:,:]
-ROMS_temperature_l0 = ROMS_temperature_l0[timestep,0,:,:]
+ROMS_temperature_l0 = ROMS_grp.variables["Tb"]
+ROMS_temperature_l0 = ROMS_temperature_l0[timestep,:,:]
+
+ROMS_temperature_lz = ROMS_grp.variables["temp"]
+print ROMS_temperature_lz.shape
+ROMS_temperature_lz10 = ROMS_temperature_lz[timestep,10,:,:]
+ROMS_temperature_lz0 = ROMS_temperature_lz[timestep,0,:,:]
 
 
-plt.subplot(1, 6, 1)
+plt.subplot(1, 7, 1)
 plt.imshow(ROMS_dBdt_l0,vmin=0.0,vmax=0.000001)
 plt.colorbar()
 plt.axis('off')  
 
-plt.subplot(1, 6, 2)
+plt.subplot(1, 7, 2)
 plt.imshow(FISOC_dBdt_l0,vmin=0.0,vmax=0.000001)
 plt.colorbar()
 plt.axis('off')  
 
-plt.subplot(1, 6, 3)
+plt.subplot(1, 7, 3)
+plt.imshow(ROMS_temperature_lz10,vmin=-2.3,vmax=-1.8)
+plt.colorbar()
+plt.axis('off')  
+
+plt.subplot(1, 7, 4)
+plt.imshow(ROMS_temperature_lz0,vmin=-2.3,vmax=-1.8)
+plt.colorbar()
+plt.axis('off')  
+
+plt.subplot(1, 7, 5)
 plt.imshow(ROMS_temperature_l0,vmin=-2.3,vmax=-1.8)
 plt.colorbar()
 plt.axis('off')  
 
-plt.subplot(1, 6, 4)
-plt.imshow(ROMS_temperature_lx,vmin=-2.3,vmax=-1.8)
-plt.colorbar()
-plt.axis('off')  
-
-plt.subplot(1, 6, 5)
+plt.subplot(1, 7, 6)
 plt.imshow(FISOC_temperature_l0,vmin=-2.3,vmax=-1.8)
 plt.colorbar()
 plt.axis('off')  
 
-plt.subplot(1, 6, 6)
+plt.subplot(1, 7, 7)
 plt.imshow(ROMS_z_l0)
 plt.colorbar()
 plt.axis('off')  
