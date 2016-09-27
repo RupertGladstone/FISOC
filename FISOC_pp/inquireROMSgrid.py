0dfabac6eb5c786f058d4a05ecab3ae9f08898d1
elmeruser <rupertgladstone1972@gmail.com>
2016-09-27 15:43:56 +0300

    .

---
+++ FISOC_pp/inquireROMSgrid.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+ROMS_grp  = Dataset("../ocean_his.nc", "r")
+
+#print ROMS_grp.variables
+
+ROMS_dBdt_l0 = ROMS_grp.variables["m"]
+xl = ROMS_grp.variables["xl"]
+el = ROMS_grp.variables["el"]
+
+print "ROMS domain length in XI direction ::: ",xl[:]/1000.," km" 
+print "ROMS domain length in ETA direction ::: ",el[:]/1000.," km" 
+
+ROMS_grp.close()
+
+
