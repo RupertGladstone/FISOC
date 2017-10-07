018d69bdfca1c5195566913bf8a6de14565ceba5
elmeruser <rupertgladstone1972@gmail.com>
2017-10-07 18:38:25 +0200

    added atmos environment vars to the example build script, but they dont do anything yet!

--- buildFISOCexample.sh
+++ buildFISOCexample.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
                                                                                                             
 export CPPFLAGS="$CPPFLAGS -D FISOC_MPI"
+#export FISOC_MPI="yes"
 
-#export FISOC_ISM="dummy"
+export FISOC_ISM="dummy"
+export FISOC_ISM_LIBS=""
+export FISOC_ISM_LIBPATH="$HOME"
+export FISOC_ISM_INCLUDE="$HOME"
+
+#export FISOC_ISM="FOOL"
 #export FISOC_ISM_LIBS=""
 #export FISOC_ISM_LIBPATH="$HOME"
 #export FISOC_ISM_INCLUDE="$HOME"
@@ -12,29 +18,37 @@ export CPPFLAGS="$CPPFLAGS -D FISOC_MPI"
 #export FISOC_ISM_LIBPATH="/usr/local/lib"
 #export FISOC_ISM_INCLUDE="$HOME"
 
-export FISOC_ISM="Elmer"
-export FISOC_ISM_LIBS="-lelmersolver"
+#export FISOC_ISM="Elmer"
 #export FISOC_ISM_LIBS="-lelmersolver -lmatc -lfhuti -larpack -lparpack"
-export FISOC_ISM_INCLUDE="$ELMER_HOME/share/elmersolver/include"
-export FISOC_ISM_LIBPATH="$ELMER_HOME/lib/elmersolver/"
+#export FISOC_ISM_INCLUDE="$ELMER_HOME/share/elmersolver/include"
+#export FISOC_ISM_LIBPATH="$ELMER_HOME/lib/elmersolver/"
 
-#export FISOC_OM="dummy"
-#export FISOC_OM_LIBS=""
-#export FISOC_OM_INCLUDE="$HOME"
-#export FISOC_OM_LIBPATH="$HOME"
+export FISOC_AM="dummy"
+export FISOC_AM_LIBS=""
+export FISOC_AM_LIBPATH="$HOME"
+export FISOC_AM_INCLUDE="$HOME"
 
-export MY_ROMS_DIR=/home/elmeruser/Source/ROMSIceShelf_devel
-export FISOC_OM="ROMS"
-export FISOC_OM_LIBS="-loceanM"
-export FISOC_OM_INCLUDE="${MY_ROMS_DIR}/Build"
-export FISOC_OM_LIBPATH="/usr/local/lib"
-#export CPPFLAGS="$CPPFLAGS -D ROMS_MASKING -D ROMS_SPHERICAL"
-export CPPFLAGS="$CPPFLAGS -D ROMS_DDDT"
+export FISOC_OM="dummy"
+export FISOC_OM_LIBS=""
+export FISOC_OM_INCLUDE="$HOME"
+export FISOC_OM_LIBPATH="$HOME"
+
+#export MY_ROMS_DIR="/home/elmeruser/Source/ROMSIceShelf_devel_MISOMIP"
+#export FISOC_OM="ROMS"
+#export FISOC_OM_LIBS="-loceanM"
+#export FISOC_OM_INCLUDE="${MY_ROMS_DIR}/Build"
+#export FISOC_OM_LIBPATH="/usr/local/lib/"
+
+# These ROMS_ preprocessor keywords correspond to a relevant subset of 
+# the preprocessor keywords in the ROMS .in file.
+#export CPPFLAGS="$CPPFLAGS -D ROMS_SPHERICAL"
+#export CPPFLAGS="$CPPFLAGS -D ROMS_MASKING"
+#export CPPFLAGS="$CPPFLAGS -D ROMS_DDDT"
 #export CPPFLAGS="$CPPFLAGS -D ROMS_DRAFT"
 
-export ESMFMKFILE="$ESMF_DIR/DEFAULTINSTALLDIR/lib/libO/Linux.gfortran.64.openmpi.default/esmf.mk"
+export ESMFMKFILE="/home/elmeruser/Source/esmf/DEFAULTINSTALLDIR/lib/libO/Linux.gfortran.64.openmpi.default/esmf.mk"
 
-make clean
+#make clean
 make install
 
 rm PET*Log
