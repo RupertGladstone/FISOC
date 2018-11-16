6ea32e42ef116b08800860daf727a110b1e379ac
elmeruser <rupertgladstone1972@gmail.com>
2018-11-16 17:29:30 +0100

    added some commented options to FISOC example build script to illustrate recent functionality

--- buildFISOCexample.sh
+++ buildFISOCexample.sh
@@ -30,6 +30,7 @@ export FISOC_ISM_INCLUDE="$HOME"
 #export FISOC_ISM_LIBS="-lelmersolver -lmatc -lfhuti -larpack -lparpack"
 #export FISOC_ISM_INCLUDE="$ELMER_HOME/share/elmersolver/include"
 #export FISOC_ISM_LIBPATH="$ELMER_HOME/lib/elmersolver/"
+export FISOC_ISM_GEOM="FISOC_ISM_MESH"
 
 export FISOC_AM="dummy"
 export FISOC_AM_LIBS=""
@@ -46,20 +47,23 @@ export FISOC_OM_LIBPATH="$HOME"
 #export FISOC_OM_LIBS="-loceanM"
 #export FISOC_OM_INCLUDE="${MY_ROMS_DIR}/Build"
 #export FISOC_OM_LIBPATH="/usr/local/lib/"
+export FISOC_OM_GEOM="FISOC_OM_GRID"
 
 # These ROMS_ preprocessor keywords correspond to a relevant subset of 
 # the preprocessor keywords in the ROMS .in file.
 #export CPPFLAGS="$CPPFLAGS -D ROMS_SPHERICAL"
 #export CPPFLAGS="$CPPFLAGS -D ROMS_MASKING"
+#export CPPFLAGS="$CPPFLAGS -D ROMS_AVERAGES"
 #export CPPFLAGS="$CPPFLAGS -D ROMS_DDDT"
+#export CPPFLAGS="$CPPFLAGS -D ROMS_DSDT"
 #export CPPFLAGS="$CPPFLAGS -D ROMS_DRAFT"
 
 export ESMFMKFILE="/home/elmeruser/Source/esmf/DEFAULTINSTALLDIR/lib/libO/Linux.gfortran.64.openmpi.default/esmf.mk"
 
-#make clean
+make clean
 make install
 
-rm PET*Log
+#rm PET*Log
 
 # to run, for example:
 # mpirun -np 4 FISOC_caller
