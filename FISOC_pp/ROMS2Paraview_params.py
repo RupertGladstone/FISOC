26b232e59d68df411abaeb47b648c5fce551fc55
Rupert Gladstone <rupertgladstone1972@gmail.com>
2019-05-01 21:29:36 +0200

    Scripts to process ROMS netcdf history files for use in Paraview. Only 2D so far.

---
+++ FISOC_pp/ROMS2Paraview_params.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+NC_inNameTemplate = "ocean_his_XXXX.nc"
+numInFiles = 1
+NC_outNameTemplate = "oh4Paraview_XXXX.nc"
+skipLen = 2 # for sub sampling in time
+timeVarName = "ocean_time"
